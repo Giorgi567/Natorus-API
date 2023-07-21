3461cb3ed0885d31d40b81acbefb2311eb36bb11
Giorgi Gvintidze <giorgininidze200@gmail.com>
2023-07-21 22:17:08 +0400

    added cors

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 
+const cors = require('cors');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -22,6 +24,9 @@ app.set('views', path.join(__dirname, 'views'));
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Cors
+app.use(cors());
 
 // Set security HTTP headers
 app.use(helmet());
